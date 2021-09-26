469c9dfa47d4d938600cd4e4b4dc48610a18f499
dinoakos <43711534+dinoakos@users.noreply.github.com>
2021-09-26 22:55:25 +0200

    Create drill2.cpp

---
+++ drill2.cpp
@@ -0,0 +1,11 @@
+#include "std_lib_facilities.h"
+int main(){
+cout << "Who do you want to write to?" 
+string first_name;
+cin >> first_name;
+cout << "Dear, " << first_name << endl;
+cout << "How are you feeling? I'm really angry, someone broke the left mirror off of the suzuki.\n"
+
+  
+return 0;
+}
