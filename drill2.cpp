630290ef4b581d1b8c9285d86296556606c60bcb
dinoakos <43711534+dinoakos@users.noreply.github.com>
2021-09-30 10:52:20 +0200

    Update drill2.cpp

--- drill2.cpp
+++ drill2.cpp
@@ -1,9 +1,9 @@
 #include "std_lib_facilities.h"
 int main(){
-cout << "Who do you want to write to? \n" 
+cout << "Who do you want to write to? \n" ;
 string first_name;
 cin >> first_name;
-cout << "Give me a name of a friend! \n" << endl;
+cout << "Give me a name of a friend! \n";
 string friend_name;
 cin >> friend_name;
 cout << "what is the gender of your friend?(m for male, f for female) \n";
@@ -11,21 +11,16 @@ char sex = '0';
 cin >> sex;
 cout << "please tell me the age of the recipiant! \n";
 int age;
-boolean checked=false;
+
 cin >> age;
-while(!checked){
+
 if(age <= 0 || age >= 110){
 simple_error("you are kidding! \n");
-}else{
-cin >> age;
-checked=true;
 }
-}
-
 
 cout << "Dear, " << first_name << endl;
 cout << "\t How are you feeling? I'm really angry, someone broke the left mirror off of the suzuki.\n";
-cout << "Have you seen " << friend_name << "lately? \n";
+cout << "Have you seen " << friend_name << " lately? \n";
 if(sex == 'f'){
 cout << "If you see " << friend_name << " please ask her to call me. \n";
 }
@@ -33,13 +28,13 @@ if(sex == 'm'){
 cout << "If you see " << friend_name << " please ask him to call me. \n";
 }
 if(age>70){
-cout >> "hope you are enjoying retirement. \n";
+cout << "Hope you are enjoying retirement. \n";
 }else{
 if(age==17){
-cout >> "you will be able to vote next year. \n"
+cout << "You will be able to vote next year. \n";
 }else{
 if(age < 12){
-cout << "next year you will be " << age+1 << " . " << endl;
+cout << "Next year you will be " << age+1 << " . " << endl;
 }
 }
 }  
