f4463a3cb1dc5f254b215e1542cc4c52bbfa49aa
dinoakos <43711534+dinoakos@users.noreply.github.com>
2021-12-13 16:12:51 +0100

    Update ch19.cpp

--- ch19.cpp
+++ ch19.cpp
@@ -1,177 +1,116 @@
-#include "../lib_files/std_lib_facilities.h"
+#include "std_lib_facilities.h"
 
-// -----------------------------------------------------------------------------
+class My_range_error{};
+template<typename T>
 
-template<class T> struct S {
-    S() : val(T()) { }
-    S(T d) : val(d) { }     // Drill 2
-    T& operator=(const T&); // Drill 10
-    T& get();               // Drill 5
-    const T& get() const;   // Drill 11
-    void set(const T& d);   // Drill 9
-private:
-    T val;
+class My_vector{
+
+	long unsigned int sz;
+	T* elem;
+	int space;
+
+public:
+	My_vector(): sz{0}, elem{nullptr}, space{0} {}
+
+	explicit My_vector(int s): sz(s), elem{new T[s]}{
+		for(int i = 0; i < s; ++i) elem[i] = 0;
+	}
+
+	My_vector(initializer_list<T> lst): sz{lst.size()}, elem{new T[sz]}{
+		copy(lst.begin(), lst.end(), elem);
+	}
+	My_vector(const My_vector& arg): sz{arg.sz}, elem{new T[arg.sz]}{
+		copy(arg.elem, arg.elem+arg.sz, elem);
+	}
+	My_vector& operator=(const My_vector& arg){
+		if(this == &arg) return *this;
+
+		if(arg.sz <= space){
+			for (int i = 0; i < arg.sz; ++i) elem[i] = arg.elem[i];
+			sz = arg.sz;
+			return *this;
+		}
+
+		T* p = new T[arg.sz];
+		copy(arg.elem, arg.elem+arg.sz, p);
+		delete[] elem;
+		elem = p;
+		space = sz = arg.sz;
+		return *this;
+	}
+	My_vector(My_vector&& arg): sz{arg.sz}, elem{arg.elem}{
+		arg.sz = 0;
+		arg.elem = nullptr;
+	}
+	My_vector& operator=(My_vector&& arg){
+
+		delete[] elem;
+		elem = arg.elem;
+		sz = arg.sz;
+		arg.elem = nullptr;
+		arg.sz = 0;
+		return *this;
+	}
+
+	~My_vector(){
+		delete[] elem;
+	}
+
+	T& operator[](int n){return elem[n];}
+	T operator[](int n) const {return elem[n];}
+
+	int size() const {return sz;}
+	int capacity() const {return space;}
+
+	void reserve(int newalloc){
+		if(newalloc <= space) return;
+
+		T* p = new T[newalloc];
+		for (int i = 0; i < sz; ++i) p[i] = elem[i];
+		delete[] elem;
+		elem = p;
+		space = newalloc;
+	}
+
+	void resize(int newsize){
+		reserve(newsize);
+		for (int i = 0; i < newsize; ++i) elem[i] = 0;
+		sz = newsize;
+	}
+
+	void push_back(T d){
+		if(space == 0)
+			reserve(8);
+		else if(sz == space)
+			reserve(2*space);
+		elem[sz] = d;
+		sz++;
+	}
+
+	T& at(int n){
+		if(n < 0 || sz <= n) throw My_range_error();
+		return elem[n];
+	}
+
+	T at(int n) const{
+		if(n < 0 || sz <= n) throw My_range_error();
+		return elem[n];
+	}
+
 };
 
-template<class T> T& S<T>::operator=(const T& d)
-{
-    val = d;
-    return val;
+int main(){
+try{
+	My_vector<string> v2;
+
+	v2.push_back("Kókuszom");
+	v2.push_back("Mókuszom");
+	v2.push_back("Hókuszom Pókuszom");
+
+	for (int i = 0; i < v2.size(); ++i)
+		cout << v2.at(i) << endl;
+	return 0;
+} catch (My_range_error){
+	cerr << "out_of_range at vector\n";
 }
-
-// Drill 6
-template<class T> T& S<T>::get() { return val; }
-
-template<class T> const T& S<T>::get() const { return val; }
-
-template<class T> void S<T>::set(const T& d) { val = d; }
-
-// -----------------------------------------------------------------------------
-
-// Drill 12
-template<class T> istream& operator>>(istream& is, S<T>& ss)
-{
-    T v;
-    cin >> v;
-    if (!is) return is;
-    ss = v;
-    return is;
-}
-
-template<class T> void read_val(T& v)
-{
-    cin >> v;
-}
-
-// -----------------------------------------------------------------------------
-
-// Drill 14
-template<class T> ostream& operator<<(ostream& os, const vector<T>& d)
-{
-    os << "{ ";
-    for (int i = 0; i<d.size(); ++i) {
-        os << d[i];
-        if (i<d.size()-1) os << ',';
-        os << ' ';
-    }
-    os << "}";
-    return os;
-}
-
-template<class T> istream& operator>>(istream& is, vector<T>& d)
-{
-    char ch1;
-    char ch2;
-    T temp;
-    vector<T> v_temp;
-    is >> ch1;
-    if (!is) return is;
-    if (ch1!='{') {
-        is.clear(ios_base::failbit);
-        return is;
-    }
-    while (cin>>temp>>ch2 && ch2==',') {
-        v_temp.push_back(temp);
-    }
-    if (ch2!='}') {
-        is.clear(ios_base::failbit);
-        return is;
-    }
-    v_temp.push_back(temp); // ch2=='}', read last value
-
-    // copy temp vector only now so d is not changed if input fails
-    d = v_temp;
-}
-
-// -----------------------------------------------------------------------------
-
-int main()
-try {
-    // Drill 3
-    S<int> s_int(5);
-    S<char> s_char('x');
-    S<double> s_dbl(3.14);
-    S<string> s_strg("String1");
-    vector<int> vi;
-    vi.push_back(1);
-    vi.push_back(2);
-    vi.push_back(3);
-    S< vector<int> > s_v_int(vi);
-
-    // Drill 4
-//    cout << "s_int: " << s_int.val << "\n";
-//    cout << "s_char: " << s_char.val << "\n";
-//    cout << "s_dbl: " << s_dbl.val << "\n";
-//    cout << "s_strg: " << s_strg.val << "\n";
-//    for (int i = 0; i<s_v_int.val.size(); ++i)
-//        cout << "s_v_int[" << i << "]: " << s_v_int.val[i] << "\n";
-
-    // Drill 8
-    cout << "s_int: " << s_int.get() << "\n";
-    cout << "s_char: " << s_char.get() << "\n";
-    cout << "s_dbl: " << s_dbl.get() << "\n";
-    cout << "s_strg: " << s_strg.get() << "\n";
-    for (int i = 0; i<s_v_int.get().size(); ++i)
-        cout << "s_v_int[" << i << "]: " << s_v_int.get()[i] << "\n";
-    cout << s_v_int.get() << "\n";
-
-    // Drill 9
-    s_int.set(55);
-    s_char.set('y');
-    s_dbl.set(4.14);
-    s_strg.set("String2");
-    vi[1] = 22;
-    s_v_int.set(vi);
-    cout << "\ns_int: " << s_int.get() << "\n";
-    cout << "s_char: " << s_char.get() << "\n";
-    cout << "s_dbl: " << s_dbl.get() << "\n";
-    cout << "s_strg: " << s_strg.get() << "\n";
-    for (int i = 0; i<s_v_int.get().size(); ++i)
-        cout << "s_v_int[" << i << "]: " << s_v_int.get()[i] << "\n";
-    cout << s_v_int.get() << "\n";
-
-    // Drill 10
-    s_int = 66;
-    s_char = 'z';
-    s_dbl = 5.14;
-    s_strg = "String3";
-    vi[1] = 33;
-    s_v_int = vi;
-    cout << "\ns_int: " << s_int.get() << "\n";
-    cout << "s_char: " << s_char.get() << "\n";
-    cout << "s_dbl: " << s_dbl.get() << "\n";
-    cout << "s_strg: " << s_strg.get() << "\n";
-    for (int i = 0; i<s_v_int.get().size(); ++i)
-        cout << "s_v_int[" << i << "]: " << s_v_int.get()[i] << "\n";
-    cout << s_v_int.get() << "\n";
-
-    // Drill 11
-    const S<int> c_s_int(5);
-    cout << "\nc_s_int: " << c_s_int.get() << "\n"; // requires const getter
-
-    // Drill 13
-    cout << "\ns_int: ";
-    read_val(s_int);
-    cout << "s_char: ";
-    read_val(s_char);
-    cout << "s_dbl: ";
-    read_val(s_dbl);
-    cout << "s_strg: ";
-    read_val(s_strg);
-    cout << "\ns_int: " << s_int.get() << "\n";
-    cout << "s_char: " << s_char.get() << "\n";
-    cout << "s_dbl: " << s_dbl.get() << "\n";
-    cout << "s_strg: " << s_strg.get() << "\n\n";
-
-    // Drill 14
-    cout << "\ns_v_int: ";
-    read_val(s_v_int);
-    cout << "\ns_v_int: " << s_v_int.get() << "\n";
-
-}
-catch (exception& e) {
-    cerr << "Exception: " << e.what() << "\n";
-}
-catch (...) {
-    cerr << "Exception\n";
 }
